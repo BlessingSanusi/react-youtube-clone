e0439863a8ed5512eb2fb1327987c48730dbe95d
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-12-16 11:41:09 -0600

    more changes

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,9 +6,13 @@ import { Grid } from '@material-ui/core'
 // function base component is a dummy base component. It is not a smart component
 
 const VideoList = ({videos}) => {
-const listOfVideo = videos.map((video) => <VideoItem />)
+const listOfVideo = videos.map((video, id) => <VideoItem key={id} video={video}/>)
 
-    return listOfVideo;
+    return (
+        <Grid container spacing={10}>
+            {listOfVideo}
+        </Grid>
+    );
 }
 
 export default VideoList
